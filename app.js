08778a27ad65c2463703e17e4035a0167a75b652
EngrBilalFeroz <bilalferoz19@gmail.com>
2023-03-19 17:30:39 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+<script>
+    let num1 = prompt("Enter the first number:");
+    let num2 = prompt("Enter the second number:");
+
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    let result = num1 * num2;
+
+    alert("The result is: " + result);
+</script>
